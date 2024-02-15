13653a3ebda4254a5ed7e1996c9c1be2d84eb055
srexrg <sreeragp276@gmail.com>
2024-02-15 15:52:21 +0530

    Message Controller Added

--- backend/server.js
+++ backend/server.js
@@ -1,24 +1,21 @@
-import express from "express"
-import dotenv from "dotenv"
-import authRoutes from "./routes/auth.routes.js"
+import express from "express";
+import dotenv from "dotenv";
+import authRoutes from "./routes/auth.routes.js";
+import messageRoutes from "./routes/message.routes.js"; 
+import userRoutes from "./routes/user.routes.js"; 
 import connectDB from "./db/db.js";
-
+import cookieParser from "cookie-parser"
 dotenv.config();
 const app = express();
-
 app.use(express.json());
-
-
-
+app.use(cookieParser())
 const port = process.env.PORT || 3000;
 
+app.use("/api/auth", authRoutes);
+app.use("/api/messages", messageRoutes);
+app.use("/api/users", userRoutes);
 
-
-app.use("/api/auth",authRoutes)
-
-
-
-app.listen(port,(req,res)=>{
+app.listen(port, (req, res) => {
   connectDB();
-  console.log(`Sever conncted on ${port}`)
-})
+  console.log(`Sever conncted on ${port}`);
+});
