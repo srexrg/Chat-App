4d979262e2231a694cbb41d42643bfa1fd7cc1b2
srexrg <sreeragp276@gmail.com>
2024-02-16 15:22:20 +0530

    Signup Add

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,16 +1,21 @@
-// import SignUp from "./pages/signup/SignUp";
-
+import SignUp from "./pages/signup/SignUp";
+import { Route, Routes } from "react-router";
 import "./App.css";
 import Home from "./pages/home/Home";
+import Login from "./pages/login/Login";
+import { Toaster } from 'react-hot-toast';
 
 function App() {
-  return(
-
-  <div className='p-4 h-screen flex items-center justify-center'>
-  {/* <SignUp/> */}
-  <Home/>
-  </div>
-  )
+  return (
+    <div className="p-4 h-screen flex items-center justify-center">
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/signup" element={<SignUp />} />
+        <Route path="/login" element={<Login />} />
+      </Routes>
+      <Toaster/>
+    </div>
+  );
 }
 
 export default App;
