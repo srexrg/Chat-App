363dc66417241aaffdb2aea1ba7f48de68cc8dc1
srexrg <sreeragp276@gmail.com>
2024-02-19 18:01:13 +0530

    Added messaging functionality

--- frontend/src/components/messages/MessageContainer.jsx
+++ frontend/src/components/messages/MessageContainer.jsx
@@ -1,37 +1,47 @@
+import { useEffect } from "react";
 import MessageInput from "./MessageInput";
 import Messages from "./Messages";
+import useConversation from "../../zustand/useConversation"
 import { TiMessages } from "react-icons/ti";
+import { useAuthContext } from "../../context/AuthContext";
 
 const MessageContainer = () => {
+  const {selectedConversation,setSelectedConversation} = useConversation()
 
-	const NochatSelected = true;
+
+  useEffect(()=>{
+    //cleanup fn (unmount)
+    return ()=>setSelectedConversation(null)
+  },[setSelectedConversation])
+
+	
   return (
-    <div className="md:min-w-[450px] flex flex-col">
-
-			{NochatSelected ?(<NoChatSelected/>
+		<div className='md:min-w-[450px] flex flex-col'>
+			{!selectedConversation ? (
+				<NoChatSelected />
 			) : (
 				<>
-        {/* Header */}
-        <div className="bg-slate-500 px-4 py-2 mb-2">
-          <span className="label-text">To:</span>{" "}
-          <span className="text-gray-900 font-bold">John doe</span>
-        </div>
-
-        <Messages />
-        <MessageInput />
-      </>
+					{/* Header */}
+					<div className='bg-slate-500 px-4 py-2 mb-2'>
+						<span className='label-text'>To:</span>{" "}
+						<span className='text-gray-900 font-bold'>{selectedConversation.fullname}</span>
+					</div>
+					<Messages />
+					<MessageInput />
+				</>
 			)}
-     
-    </div>
-  );
+		</div>
+	);
 };
 export default MessageContainer;
 
 const NoChatSelected = () => {
+
+	const {authUser} = useAuthContext();
   return (
     <div className="flex items-center justify-center w-full h-full">
       <div className="px-4 text-center sm:text-lg md:text-xl text-gray-200 font-semibold flex flex-col items-center gap-2">
-        <p>Welcome 👋 John Doe ❄</p>
+        <p>Welcome 👋 {authUser.username} ❄</p>
         <p>Select a chat to start messaging</p>
         <TiMessages className="text-3xl md:text-6xl text-center" />
       </div>
