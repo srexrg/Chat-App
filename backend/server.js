05a3a7ad585dd75f4b8c3b89d0475a2f61b68a3f
srexrg <sreeragp276@gmail.com>
2024-04-14 18:14:57 +0530

    DB Connection Changes

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,6 @@
 import path from "path";
 import express from "express";
+import cors from "cors"
 import dotenv from "dotenv";
 import authRoutes from "./routes/auth.routes.js";
 import messageRoutes from "./routes/message.routes.js";
@@ -13,6 +14,7 @@ import job from "./cron.js";
 dotenv.config();
 app.use(express.json());
 app.use(cookieParser());
+app.use(cors())
 const port = process.env.PORT || 3000;
 
 // job.start();
